26580c6c6e71f3747cb5a28b07a13740ecaa6aa0
aidanabekova <bekovaaidana081003@gmail.com>
2022-06-14 21:07:04 +0600

    hw3

--- hw3/hw3.js
+++ hw3/hw3.js
@@ -1,3 +1,6 @@
+// Задание 1:
+// Сделать функцию котороя принимает 2 number аргумента
+// И вычисляет какой из них меньше
 function Comparison () {
     let num1 = prompt('Number 1: ')
     let num2 = prompt('Number 2: ')
@@ -10,7 +13,9 @@ function Comparison () {
     }
 }
 Comparison()
-
+// Задание 2:
+// Написать функцию countChar
+// Которая может считать длину переданной как аргумент
 function countChar () {
     let argument = prompt('Введите что-нибудь, без разницы что')
     console.log(`Длина аргумента ${argument.length} символов`)
